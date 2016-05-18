5cae8ce4957b775c8ca37f4cbb85d7e988699668
sergioarc <sergioar@ciencias.unam.mx>
2016-05-18 14:27:20 -0500

    Se agrego el codigo del xml

---
+++ Unity/src/EQ_beta1.0/Assets/Scripts/Elemento.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Xml;
+
+//Objeto que crea un elemento con su respectiva información cargada desde un xml
+public class Elemento : MonoBehaviour {
+
+	public string nombre = ""; //Nombre del elemento
+	string simbolo = ""; //Símbolo atómico
+	string descripElem = ""; //Descripción del elemento
+	string descripModAto = ""; //Descripción del model atómico
+	string usoApli = ""; //Uso y aplicaciones 
+	string fuente = ""; //fuente
+	float masaAtomica = 0; //Masa atómica
+	int numeroAtomico = 0; //Número atómico
+
+	//Método constructor que crea un elemento con su respectivos valores de una rchivo fuente xml
+	public Elemento(string nombre){
+		//Leer xml y asignarlos a cada atributo del elemento
+
+		XmlReader reader = XmlReader.Create("Assets/xml/Config.xml");
+
+		while (reader.Read()){
+			if (reader.NodeType == XmlNodeType.Element && reader.Name == "Elementos"){
+				while (reader.NodeType != XmlNodeType.EndElement){
+					reader.Read();
+					//Identifica el nodo del elemento
+					if (reader.Name == nombre){
+						while (reader.NodeType != XmlNodeType.EndElement){
+							reader.Read();
+							//Identifica el atributo nombre
+							if(reader.Name == "Nombre"){
+								while (reader.NodeType != XmlNodeType.EndElement){
+									reader.Read();
+									if (reader.NodeType == XmlNodeType.Text){
+										this.nombre = reader.Value;
+										print("Nombre: " + this.nombre);
+									}
+								}
+								reader.Read();
+							}
+							//Identifica el atributo Símbolo atómico
+							if(reader.Name == "SimboloAtomico"){
+								while (reader.NodeType != XmlNodeType.EndElement){
+									reader.Read();
+									if (reader.NodeType == XmlNodeType.Text){
+										this.simbolo = reader.Value;
+										print("Símbolo Atómico: " + this.simbolo);
+									}
+								}
+								reader.Read ();
+							}
+
+							//Identifica el atributo Descripción del elemento
+							if(reader.Name == "DescripcionElemento"){
+								while (reader.NodeType != XmlNodeType.EndElement){
+									reader.Read();
+									if (reader.NodeType == XmlNodeType.Text){
+										this.descripElem = reader.Value;
+										print("Descripción del elemento: " + this.descripElem);
+									}
+								}
+								reader.Read ();
+							}
+
+							//Identifica el atributo Descripción del modelo atómico
+							if(reader.Name == "DescripcionModeloAtomico"){
+								while (reader.NodeType != XmlNodeType.EndElement){
+									reader.Read();
+									if (reader.NodeType == XmlNodeType.Text){
+										this.descripModAto = reader.Value;
+										print("Descripción del modelo atómico: " + this.descripModAto);
+									}
+								}
+								reader.Read ();
+							}
+
+							//Identifica el atributo uso y aplicaciones
+							if(reader.Name == "UsosAplicaciones"){
+								while (reader.NodeType != XmlNodeType.EndElement){
+									reader.Read();
+									if (reader.NodeType == XmlNodeType.Text){
+										this.usoApli = reader.Value;
+										print("Uso y aplicaciones: " + this.usoApli);
+									}
+								}
+								reader.Read ();
+							}
+
+							//Identifica el atributo Masa atómica
+							if(reader.Name == "MasaAtomica"){
+								while (reader.NodeType != XmlNodeType.EndElement){
+									reader.Read();
+									if (reader.NodeType == XmlNodeType.Text){
+										this.masaAtomica = Single.Parse(reader.Value);
+										print("Masa atómica: " + this.masaAtomica);
+									}
+								}
+								reader.Read ();
+							}
+
+							//Identifica el atributo Número atómico
+							if(reader.Name == "NumeroAtomico"){
+								while (reader.NodeType != XmlNodeType.EndElement){
+									reader.Read();
+									if (reader.NodeType == XmlNodeType.Text){
+										this.numeroAtomico = Int32.Parse(reader.Value);
+										print("Número atómico: " + this.numeroAtomico);
+									}
+								}
+								reader.Read ();
+							}
+
+							//Identifica el atributo Número atómico
+							if(reader.Name == "Fuente"){
+								while (reader.NodeType != XmlNodeType.EndElement){
+									reader.Read();
+									if (reader.NodeType == XmlNodeType.Text){
+										this.fuente = reader.Value;
+										print("Fuente: " + this.fuente);
+									}
+								}
+								reader.Read ();
+							}
+						}
+					} 
+				}
+			}
+
+		}
+	}
+
+	void Start(){
+		Debug.Log ("Hola mundo");
+		new Elemento ("Oro");
+	}
+}
--- Unity/src/EQ_beta1.0/Assets/Scripts/LoadingBar.cs
+++ Unity/src/EQ_beta1.0/Assets/Scripts/LoadingBar.cs
@@ -4,30 +4,30 @@ using System.Collections;
 
 public class LoadingBar : MonoBehaviour {
 
-	public Transform LoadBar = null;
-	public Transform TextIndicator = null;
-	public Transform TextLoading = null;
-	[SerializeField] private float currentAmount;
-	[SerializeField] private float speed;
+	public Transform LoadBar = null; //Componenete del load bar
+	public Transform TextIndicator = null; //Componente del texto indicador
+	public Transform TextLoading = null; //Componente del cargador
+	[SerializeField] private float currentAmount; //Valor actual del porcentaje
+	[SerializeField] private float speed; //Velocidad de carga
 
 
 	// Update is called once per frame
 	void Update () {
 
-		//Verifica que la
-		if (currentAmount < 100) {
-			print ("Current: " + currentAmount);
+		//Verifica que el valor actual del porcentaje
+		if (currentAmount < 100) { //Si es menor a 100, entonces aumenta la cantidad actual
+			//print ("Current: " + currentAmount);
 			currentAmount += speed * Time.deltaTime;
-			print ("Current: " + currentAmount + " Speed: " + speed);
+			//print ("Current: " + currentAmount + " Speed: " + speed);
 			TextIndicator.GetComponent<Text>().text = ((int)currentAmount).ToString() + "%";
 			TextLoading.gameObject.SetActive (true);
-		} else {
-			print ("Current: " + currentAmount);
+		} else { //Si es mayor a 100 a terminado de cargar la aplicacion y cargamos una nueva escena
+			//print ("Current: " + currentAmount);
 			TextLoading.gameObject.SetActive (false);
-			TextIndicator.GetComponent<Text>().text = "FIN";
+			TextIndicator.GetComponent<Text>().text = "100%";
 			Application.LoadLevel ("PantallaInicio");
 		}
-
-		LoadBar.GetComponent<Image> ().fillAmount = currentAmount / 100;
+		//Pintamos el componente loadbar con respecto al total
+		LoadBar.GetComponent<Image> ().fillAmount = currentAmount / 100; 
 	}
 }
