865945d2995deed9795ed4a01cd503da9bd3a5a7
omarrugu <omega2049@hotmail.com>
2016-05-19 13:23:10 -0500

    Implemente diseño y funcionalidades

---
+++ Unity/src/EQ_beta1.0/Assets/Scripts/escalarNewBehaviourScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class escalarNewBehaviourScript : MonoBehaviour {
+
+    public Transform WorldObject;
+    public Transform CanvasObject;
+
+    int CachedWidth;
+    int CachedHeight;
+
+    void Refresh()
+    {
+        CachedWidth = Screen.width;
+        CachedHeight = Screen.height;
+
+        WorldObject.localScale = CanvasObject.localScale;
+    }
+
+    void Start()
+    {
+        Refresh();
+    }
+
+    void Update()
+    {
+        if (Screen.width == CachedWidth)
+        {
+            if (Screen.height == CachedHeight)
+            {
+                return;
+            }
+        }
+
+        Refresh();
+    }
+}
